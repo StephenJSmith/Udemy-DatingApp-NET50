b82bb359336ccc62d46b080b3aacce699ddc0938
Stephen Smith <stephensmith_aus@yahoo.com.au>
2020-09-27 10:30:40 +1000

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,6 +6,12 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using API.Data;
+using API.Services;
+using API.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using API.Extensions;
 
 namespace API
 {
@@ -21,17 +27,10 @@ namespace API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddDbContext<DataContext>(options =>
-      {
-        options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-      });
+      services.AddApplicationServices(_config);
       services.AddControllers();
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-      });
-
       services.AddCors();
+      services.AddIdentityServices(_config);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,8 +48,11 @@ namespace API
       app.UseRouting();
 
       app.UseCors(x =>
-          x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+          x.AllowAnyHeader()
+          .AllowAnyMethod()
+          .WithOrigins("https://localhost:4200"));
 
+      app.UseAuthentication();
       app.UseAuthorization();
 
       app.UseEndpoints(endpoints =>
